690c7bf16a7dc1c04d62768a6626fcf51f1c676e
alicia0117 <alicia0117@naver.com>
2023-11-21 16:34:00 +0900

    화요일 강의_학교

--- board.c
+++ board.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "board.h"
 
 #define N_BOARD     15
-#define BOARDSTATUS_OK     1
-#define BOARDSTATUS_NOK    0
+#define N_COINPOS   12
+#define N_MAX_COIN  4
 
 static int board_status[N_BOARD];
 static int board_coin[N_BOARD];
 
 static int board_sharkPosition;
 
+int board_initboard(void)
 {
+    int i;
+    for (i=0; i<N_BOARD; i++)
+    {
+        board_status[i] = BOARDSTATUS_OK;
+        board_coin[i] = 0;
+     }
+     
+    for (i = 0; i<N_COINPOS; i++)
+    {
+        int flag = 0;
+        while(flag == 0)
+        {
+        while()
+        {
+               int allocPos = rand()%N_BOARD;
+               if(board_coin[allocPos] == 0)
+               {
+                  board_coin[allocPos] = rand()%MAX_COIN+1;
+                  }        
+        
+}
        
 }
 
@@ -30,6 +53,8 @@ int board_printBoardstatus(void)
      }
      printf("|\n");
      printf("--------------------------------------------------\n");
+     
+     return 0;
      
  }
 
